c70058849a2b112d54cdd744f046b06662a266bb
nmsderp <130254323+nmsderp@users.noreply.github.com>
2023-07-29 22:31:50 -0400

    Update docusaurus.config.js

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -13,7 +13,6 @@ module.exports = {
       title: 'Snail IDE Documentation',
       items: [
         {
-        {
           href: 'https://snail-ide.github.io/',
           label: 'Snail IDE',
           position: 'right'
